fb2837619c1cd8bfc0cbf8b8137db3a5c02bca73
B-e-sa <joaospereira963@gmail.com>
2025-03-19 00:34:55 -0300

    add price constructor to DeliveryType values

--- src/main/java/project/flux/api/v1/models/common/enums/DeliveryType.java
+++ src/main/java/project/flux/api/v1/models/common/enums/DeliveryType.java
@@ -1,8 +1,17 @@
 package project.flux.api.v1.models.common.enums;
 
 public enum DeliveryType {
-	ECONOMICAL,
-	FAST,
-	CONVENTIONAL,
-	COLLECT
+	ECONOMICAL(1.15),
+	CONVENTIONAL(1.45),
+	FAST(1.75);
+
+    private final double price;
+
+    DeliveryType(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
