343e3e23ef51a915fde838bd7fa27965741e5754
B-e-sa <joaospereira963@gmail.com>
2025-03-20 21:36:09 -0300

    move business logic from CarrierDTO to Carrier entity

--- src/main/java/project/flux/api/v1/models/Carrier.java
+++ src/main/java/project/flux/api/v1/models/Carrier.java
@@ -1,19 +1,32 @@
 package project.flux.api.v1.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 import project.flux.api.v1.models.common.DatabaseEntity;
+import project.flux.api.v1.models.common.enums.DeliveryType;
 
 @Entity
 @Getter
 @Setter
 public class Carrier extends DatabaseEntity {
+	@Column(nullable = false, unique = true)
 	private String name;
+	
+	@Column(name = "delivery_type", nullable = false)
+	private DeliveryType deliveryType;
+	
+	@Column(name = "base_tax", nullable = false)
+	private double baseTax;
 	
 	public Carrier() {}
 	
-	public Carrier(String name) {
+	public Carrier(String name, DeliveryType deliveryType, double baseTax) {
 		this.name = name;
+		this.deliveryType = deliveryType;
+		this.baseTax = baseTax != 0 
+				? baseTax 
+				: 20 * Math.random() * this.deliveryType.getPrice();
 	}
 }
--- src/main/java/project/flux/api/v1/services/dtos/CarrierDTO.java
+++ src/main/java/project/flux/api/v1/services/dtos/CarrierDTO.java
@@ -1,17 +1,34 @@
-package project.flux.api.v1.models.dtos;
+package project.flux.api.v1.services.dtos;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import project.flux.api.v1.models.Carrier;
+import project.flux.api.v1.models.common.decorators.EnumValidator;
+import project.flux.api.v1.models.common.enums.DeliveryType;
 
 @Getter
+@AllArgsConstructor
 public class CarrierDTO {
+	private long id;
+	
 	@NotNull
 	private String name;
 	
+	private double baseTax;
+	
+	@NotNull
+	@EnumValidator(enumClass = DeliveryType.class)
+	private String deliveryType;
+	
 	public CarrierDTO() {}
 	
-	public CarrierDTO(String name) {
+	public CarrierDTO(long id, 
+			String name, 
+			String deliveryType, 
+			double baseTax) {
+		this.id = id;
 		this.name = name;
+		this.deliveryType = deliveryType;
+		this.baseTax = baseTax;
 	}
 }
